4f0bf3b3b2f1519003adc8c5e08d4a8c9add9b19
LiliyaFaizulina <lily.fed85@gmail.com>
2023-02-17 18:57:29 +0200

    add container for home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,24 +5,26 @@ const HomePage = () => {
   const mainColor = 'primary';
   return (
     <section className="section">
-      <h1 className="title has-text-centered is-size-4-desktop  is-family-monospace is-italic">
-        Rat race
-      </h1>
-      <div
-        className={`box has-background-${mainColor}-light has-text-${mainColor}-dark  is-size-4-desktop`}
-      >
-        <p>
-          Welcome to Rat race App! <br />
-          This app has been designed to improve the usability of the Rat Race
-          board game. Here you can create a player, change information about a
-          job title and dream, add information about income by categories,
-          expenses by categories, credits and equities, change financial info.
-          And App will calculate your total income, total cash flow, total
-          expenses and receivables. To start{' '}
-          <Link to={PATH_ROUTES.register}>register</Link> new player or{' '}
-          <Link to={PATH_ROUTES.login}>login</Link> if you have an account and
-          have fun!
-        </p>
+      <div className="container">
+        <h1 className="title has-text-centered is-size-4-desktop  is-family-monospace is-italic">
+          Rat race
+        </h1>
+        <div
+          className={`box has-background-${mainColor}-light has-text-${mainColor}-dark  is-size-4-desktop`}
+        >
+          <p>
+            Welcome to Rat race App! <br />
+            This app has been designed to improve the usability of the Rat Race
+            board game. Here you can create a player, change information about a
+            job title and dream, add information about income by categories,
+            expenses by categories, credits and equities, change financial info.
+            And App will calculate your total income, total cash flow, total
+            expenses and receivables. To start{' '}
+            <Link to={PATH_ROUTES.register}>register</Link> new player or{' '}
+            <Link to={PATH_ROUTES.login}>login</Link> if you have an account and
+            have fun!
+          </p>
+        </div>
       </div>
     </section>
   );
